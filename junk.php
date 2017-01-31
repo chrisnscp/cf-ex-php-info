39a9f1a2906d4841826e8edb32a3e3cb11335193
chrisnscp <christian.schutz@hpe.com>
2017-01-31 11:31:36 +0100

    Update junk.php

--- junk.php
+++ junk.php
@@ -1,6 +1,6 @@
 <?php
     print "Include Path: " . get_include_path() . "\n";
     require("test.php");
-    print "Hello World!!!\n";
+    print "Hello World from Grenoble !!!\n";
     print helloWorld() . "\n";
 ?>
